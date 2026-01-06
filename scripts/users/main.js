df9e1deff4d55810c5bdd1fafd2dbd5e53b796ac
ashishnegi <ashishsinghnegi381@gmail.com>
2026-01-06 10:39:50 +0530

    wip:adding secondary countries in user update

--- scripts/users/main.js
+++ scripts/users/main.js
@@ -1,18 +1,23 @@
 import * as fs from "node:fs/promises";
+// import userData from "../../../kovon/resourse/prachiUserListUpdated/userListUpdatesByPrachi_2Jan25.json";
+import userData from "../../../kovon/resourse/prachiUserListUpdated/userListUpdatesByPrachi_5Jan25.json" with { type: "json" };
+import { connect } from "../../mongoose/mongoose.js";
 import { createUserInCognito } from "./createUserInCognito.js";
 import { patchUserDirectlyDB } from "./patchUserDirectlyDB.js";
-import userData from "../../../kovon/resourse/prachiUserListUpdated/userListUpdatesByPrachi_2Jan25.json" with { type: "json" };
-import { connect } from "../../mongoose/mongoose.js";
-const now = Date.now()
+const now = Date.now();
 export async function writeFailureMessage({ message }) {
-  await fs.appendFile("./response/failedUserCreationLogs"+now+".txt", `${message}\n`, {
-    flag: "a",
-  });
+  await fs.appendFile(
+    "./response/failedUserCreationLogs" + now + ".txt",
+    `${message}\n`,
+    {
+      flag: "a",
+    }
+  );
 }
 
 export async function writeSuccessMessage({ message }) {
   await fs.appendFile(
-    "./response/successUserCreationLogs"+now+".txt",
+    "./response/successUserCreationLogs" + now + ".txt",
     `${message}\n`,
     { flag: "a" }
   );
@@ -20,14 +25,13 @@ export async function writeSuccessMessage({ message }) {
 
 async function main() {
   // console.log("clearing the previous logs")
-   
   console.log("Starting the creation of User in cognito and db");
   const cognitoUrl = process.env.COGNITO_URL;
-  const candidateUrl = process.env.CANDIDATE_URL;
+  const candidateUrl = process.env.CANDIDATE_URL_DEV;
   const ClientId = process.env.CLIENT_ID;
-    await connect();
+  await connect();
 
-  if (!(cognitoUrl  || candidateUrl || ClientId)) {
+  if (!(cognitoUrl || candidateUrl || ClientId)) {
     console.log("required variables are not available check the env file");
     return;
   }
@@ -39,25 +43,36 @@ async function main() {
         cognitoUrl,
         ClientId,
       });
+
       const payload = {
-        phoneNumber:user.phoneNumber,
+        phoneNumber: user.phoneNumber,
         fullName: user.fullName,
-        targetCountry:{
-          name:user["targetCountry.name"],
-          id:user["targetCountry.name_id"]
+        targetCountry: {
+          name: user["targetCountry.name"],
+          id: user["targetCountry.name_id"],
         },
-        targetJobRole:{
-          name:user["targetJobRole.name"],
-          id:user["targetJobRole._id"]
-        }
+        targetJobRole: {
+          name: user["targetJobRole.name"],
+          id: user["targetJobRole._id"],
+        },
       };
+
+      if (user.secondaryJobRoles) {
+        payload.secondaryJobRoles = user.secondaryJobRoles_raw
+          ? user.secondaryJobRoles_raw.split("|").map((item) => {
+              const [id, name] = item.split(":");
+              return { id, name };
+            })
+          : [];
+      }
+
       await patchUserDirectlyDB({ body: payload });
     } catch (error) {
       // await writeFailureMessage({message: error});
-      console.log(error)
+      console.log(error);
     }
   }
-  return
+  return;
 }
 
 await main();
